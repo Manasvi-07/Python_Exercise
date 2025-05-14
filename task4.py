f1c7ac4266fdc6590a31dc63137983d9c24a0a3c
Mac <mac@Taglines-iMac-Pro-106.local>
2025-05-14 14:48:20 +0530

    Added task 4

---
+++ task4.py
@@ -0,0 +1,24 @@
+# ---------- n-th fibonacci number using Recursion 
+
+def fibonacci(n, num={}):
+    if n in num:
+        return num[n]
+    if n <= 2:
+        return 1
+    num[n] = fibonacci(n-1, num) + fibonacci(n-2, num)
+    return num[n]
+
+n = int(input("Enter number : "))
+print("Fibonacci number : ", fibonacci(n))    
+
+
+# ------- Number of sum using Recursion
+
+def sum_Number(Number):
+    if not Number:
+        return 0
+    return Number[0] + sum_Number(Number[1:])
+
+Number = [23,44,5,67,1,1,2,4,5]
+print("Sum of Number : ", sum_Number(Number))
+
